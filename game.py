079bf6a643053b7b696de8bf1f6775f53689d6a2
sheldonu <uch20002@byui.edu>
2024-04-04 10:32:46 -0700

    player added

--- game.py
+++ game.py
@@ -1,5 +1,6 @@
 import arcade
 from arcade.tilemap import read_tmx
+from arcade.physics_engines import PhysicsEngineSimple
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
@@ -18,6 +19,27 @@ class GameWindow(arcade.Window):
 
     def setup(self):
         self.level = read_tmx("level.tmx")
+
+
+
+class GameWindow(arcade.Window):
+    # ...
+
+    def setup(self):
+        # ...
+
+self.physics_engine = PhysicsEngineSimple(self.player_sprite, self.level["Platforms"])
+
+
+class Player(arcade.Sprite):
+    def __init__(self, image, scale):
+        super().__init__(image, scale)
+
+    def update(self):
+        # Update player movement and animations here
+        pass
+
+
 
 
 if __name__ == "__main__":
